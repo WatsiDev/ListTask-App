b24dbb005ac411cc0912d72ed74fcc109f87e61b
JairVazquez <jairvazquez604@gmail.com>
2024-09-29 18:55:46 -0600

    Add bg responsive

--- app/src/main/java/com/watsidev/tasklist/MainActivity.kt
+++ app/src/main/java/com/watsidev/tasklist/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -206,9 +207,11 @@ fun TaskList(modifier: Modifier) {
             .fillMaxSize()
     ) {
         Image(
-            painter = painterResource(id = R.drawable.bgimg),
+            painter = painterResource(id = R.drawable.bglarge),
             contentDescription = null,
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .fillMaxSize()
         )
     }
 
